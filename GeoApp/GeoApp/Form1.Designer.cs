36d1a76f3b4550b93795d372546507c64317a8d3
Caleb Gosiak <cgosiak@atomiclearning.com>
2016-02-29 18:49:17 -0600

    File Diologue Work

--- GeoApp/GeoApp/Form1.Designer.cs
+++ GeoApp/GeoApp/Form1.Designer.cs
@@ -30,16 +30,24 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.runProTab = new System.Windows.Forms.TabPage();
-            this.individual = new System.Windows.Forms.TabPage();
-            this.teams = new System.Windows.Forms.TabPage();
+            this.getFileButton = new System.Windows.Forms.Button();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.calcScores = new System.Windows.Forms.Button();
+            this.results = new System.Windows.Forms.TabPage();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.individualTab = new System.Windows.Forms.TabPage();
+            this.teamTab = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
+            this.runProTab.SuspendLayout();
+            this.results.SuspendLayout();
+            this.tabControl2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.runProTab);
-            this.tabControl1.Controls.Add(this.individual);
-            this.tabControl1.Controls.Add(this.teams);
+            this.tabControl1.Controls.Add(this.results);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tabControl1.Location = new System.Drawing.Point(12, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -48,33 +56,92 @@
             // 
             // runProTab
             // 
-            this.runProTab.Location = new System.Drawing.Point(4, 25);
+            this.runProTab.Controls.Add(this.getFileButton);
+            this.runProTab.Controls.Add(this.fileLabel);
+            this.runProTab.Controls.Add(this.calcScores);
+            this.runProTab.Location = new System.Drawing.Point(4, 29);
             this.runProTab.Name = "runProTab";
             this.runProTab.Padding = new System.Windows.Forms.Padding(3);
-            this.runProTab.Size = new System.Drawing.Size(744, 368);
+            this.runProTab.Size = new System.Drawing.Size(744, 364);
             this.runProTab.TabIndex = 0;
             this.runProTab.Text = "Main";
             this.runProTab.UseVisualStyleBackColor = true;
             // 
-            // individual
+            // getFileButton
             // 
-            this.individual.Location = new System.Drawing.Point(4, 25);
-            this.individual.Name = "individual";
-            this.individual.Padding = new System.Windows.Forms.Padding(3);
-            this.individual.Size = new System.Drawing.Size(744, 368);
-            this.individual.TabIndex = 1;
-            this.individual.Text = "Individual Results";
-            this.individual.UseVisualStyleBackColor = true;
+            this.getFileButton.Location = new System.Drawing.Point(314, 113);
+            this.getFileButton.Name = "getFileButton";
+            this.getFileButton.Size = new System.Drawing.Size(125, 30);
+            this.getFileButton.TabIndex = 2;
+            this.getFileButton.Text = "Open File";
+            this.getFileButton.UseVisualStyleBackColor = true;
+            this.getFileButton.Click += new System.EventHandler(this.getFileButton_Click);
             // 
-            // teams
+            // fileLabel
             // 
-            this.teams.Location = new System.Drawing.Point(4, 25);
-            this.teams.Name = "teams";
-            this.teams.Padding = new System.Windows.Forms.Padding(3);
-            this.teams.Size = new System.Drawing.Size(744, 368);
-            this.teams.TabIndex = 2;
-            this.teams.Text = "Team Results";
-            this.teams.UseVisualStyleBackColor = true;
+            this.fileLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.fileLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileLabel.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.fileLabel.Location = new System.Drawing.Point(83, 80);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(586, 30);
+            this.fileLabel.TabIndex = 1;
+            this.fileLabel.Text = "Please Select File to Read Scores";
+            this.fileLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // calcScores
+            // 
+            this.calcScores.AutoSize = true;
+            this.calcScores.BackColor = System.Drawing.Color.Transparent;
+            this.calcScores.Location = new System.Drawing.Point(563, 318);
+            this.calcScores.Name = "calcScores";
+            this.calcScores.Size = new System.Drawing.Size(175, 40);
+            this.calcScores.TabIndex = 0;
+            this.calcScores.Text = "Calcualte Scores";
+            this.calcScores.UseVisualStyleBackColor = false;
+            this.calcScores.Click += new System.EventHandler(this.calcScores_Click);
+            // 
+            // results
+            // 
+            this.results.Controls.Add(this.tabControl2);
+            this.results.Location = new System.Drawing.Point(4, 29);
+            this.results.Name = "results";
+            this.results.Padding = new System.Windows.Forms.Padding(3);
+            this.results.Size = new System.Drawing.Size(744, 364);
+            this.results.TabIndex = 1;
+            this.results.Text = "Results";
+            this.results.UseVisualStyleBackColor = true;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.individualTab);
+            this.tabControl2.Controls.Add(this.teamTab);
+            this.tabControl2.Location = new System.Drawing.Point(-4, 3);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(752, 365);
+            this.tabControl2.TabIndex = 0;
+            // 
+            // individualTab
+            // 
+            this.individualTab.Location = new System.Drawing.Point(4, 29);
+            this.individualTab.Margin = new System.Windows.Forms.Padding(0);
+            this.individualTab.Name = "individualTab";
+            this.individualTab.Padding = new System.Windows.Forms.Padding(3);
+            this.individualTab.Size = new System.Drawing.Size(744, 332);
+            this.individualTab.TabIndex = 0;
+            this.individualTab.Text = "Individual";
+            this.individualTab.UseVisualStyleBackColor = true;
+            // 
+            // teamTab
+            // 
+            this.teamTab.Location = new System.Drawing.Point(4, 29);
+            this.teamTab.Name = "teamTab";
+            this.teamTab.Padding = new System.Windows.Forms.Padding(3);
+            this.teamTab.Size = new System.Drawing.Size(744, 332);
+            this.teamTab.TabIndex = 1;
+            this.teamTab.Text = "Team";
+            this.teamTab.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -85,6 +152,10 @@
             this.Name = "Form1";
             this.Text = "Geo Competition";
             this.tabControl1.ResumeLayout(false);
+            this.runProTab.ResumeLayout(false);
+            this.runProTab.PerformLayout();
+            this.results.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -93,8 +164,13 @@
 
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage runProTab;
-        private System.Windows.Forms.TabPage individual;
-        private System.Windows.Forms.TabPage teams;
+        private System.Windows.Forms.TabPage results;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage individualTab;
+        private System.Windows.Forms.TabPage teamTab;
+        private System.Windows.Forms.Button calcScores;
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.Button getFileButton;
     }
 }
 
--- GeoApp/GeoApp/Form1.cs
+++ GeoApp/GeoApp/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace GeoApp
 {
     public partial class Form1 : Form
     {
+        // Public Variables
+        public bool input_file_available = false; // Does not become true until user selects a file to read from
+        public string input_file_name; // The input text file to parse for scores
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void calcScores_Click(object sender, EventArgs e)
+        {
+            if (input_file_available)
+            {
+                int x = 0;
+            }
+            else
+            {
+                MessageBox.Show("Please select an input file first.");
+            }
+        }
+
+        private void getFileButton_Click(object sender, EventArgs e)
+        {
+            Stream my_stream = null;
+            OpenFileDialog input_file_dialogue = new OpenFileDialog();
+
+            // input_file_dialogue.InitialDirectory = "c:\\";
+            input_file_dialogue.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            input_file_dialogue.FilterIndex = 2;
+            input_file_dialogue.RestoreDirectory = true;
+
+            if (input_file_dialogue.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((my_stream = input_file_dialogue.OpenFile()) != null)
+                    {
+                        input_file_name = input_file_dialogue.FileName.ToString(); // sets the usable file
+                        input_file_available = true; // file is now available
+                        fileLabel.Text = input_file_name;
+                        my_stream.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
         }
     }
 }
